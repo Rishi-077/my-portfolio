eaf89b7d58db9d1deabfc6ca7931d49f760d19af
Rishi-077 <rishikesan070@gmail.com>
2023-12-08 22:51:25 +0530

    Home responsive completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,65 @@
+import { Link } from "react-scroll";
 import images from "../../constants";
 import Navbar from "../../layouts/Navbar/Navbar";
 import styles from "./Home.module.css";
+import { IoMailOutline } from "react-icons/io5";
+import { useState } from "react";
 
 const Home = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <>
       <section className={`container-fluid ${styles.home_container}`}>
         <Navbar />
 
+        <div className={`position-relative`}>
+          <div className={`${styles.hamburger_menu}`}>
+            <button
+              className="d-flex align-items-center"
+              onClick={() => setNav(!nav)}
+            >
+              <span className="uc px-1">Menu</span>{" "}
+              <div>
+                <div className={`${styles.bar}`}></div>
+                <div className={`${styles.bar}`}></div>
+                <div className={`${styles.bar}`}></div>
+              </div>
+            </button>
+
+            <div
+              style={{
+                maxHeight: nav ? "200px" : 0,
+                transition: "max-height 0.5s ease-in-out",
+              }}
+              className={`${styles.nav_route_active} d-md-none d-block`}
+            >
+              <ul className={`${styles.nav_ul}`}>
+                <li onClick={() => setNav(!nav)}>
+                  <Link>Home</Link>
+                </li>
+                <li onClick={() => setNav(!nav)}>
+                  <Link>Work</Link>
+                </li>
+                <li onClick={() => setNav(!nav)}>
+                  <Link>Service</Link>
+                </li>
+                <li onClick={() => setNav(!nav)}>
+                  <Link>Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
         <div className={"row " + styles.home_parent_div}>
-          <div className="col-5 d-flex align-items-center justify-content-end">
-            <div className={`${styles.home_img_div} me-5`}>
+          <div className="col-md-5 col-12 d-flex align-items-center justify-content-md-end justify-content-start">
+            <div className={`${styles.home_img_div} me-lg-5 me-0`}>
               <img src={images.profile} alt="img" />
             </div>
           </div>
-          <div className="col-7 d-flex align-items-center justify-content-start">
-            <div className={`${styles.home_content_div}`}>
+          <div className="col-md-7 col-12  d-flex align-items-center justify-content-start mt-md-0 mt-4">
+            <div className={`${styles.home_content_div} ms-lg-0 ms-md-5 ms-0`}>
               <h1 className={``}>
                 My name is Rishi.
                 <br />
@@ -26,7 +70,58 @@ const Home = () => {
           </div>
         </div>
 
-        <div></div>
+        <div className={"container " + styles.home_detail_parent}>
+          <div className="row">
+            <div className="col-lg-4 col-12 d-flex justicy-content-center">
+              <div className={`${styles.home_details}`}>
+                <h6>Development</h6>
+                <h3>Web (ReactJs and Angular)</h3>
+              </div>
+            </div>
+            <div className="col-lg-4 col-12 mt-lg-0 mt-sm-5 mt-3 d-flex flex-sm-row flex-column justicy-content-lg-center justify-content-between">
+              <div className={`${styles.home_details} text-start text-lg-end`}>
+                <h6>Phone</h6>
+                <h3>+91 93610 87223</h3>
+              </div>
+              <div
+                className={`${styles.home_details} mt-sm-0 mt-3 d-flex d-lg-none`}
+              >
+                <div className="me-3 text-start text-lg-end">
+                  <h6 className={`${styles.email_h6}`}>Drop your Message</h6>
+                  <h3 className={`${styles.email_h3}`}>
+                    <a href="rishikesan070@gmail.com">
+                      rishikesan070@gmail.com
+                    </a>
+                  </h3>
+                </div>
+
+                <div className="d-flex align-items-end">
+                  <div className={`${styles.icon_div}`}>
+                    <IoMailOutline />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 d-lg-flex d-none justicy-content-center">
+              <div className={`${styles.home_details} d-flex`}>
+                <div className="me-3  text-end">
+                  <h6 className={`${styles.email_h6}`}>Drop your Message</h6>
+                  <h3 className={`${styles.email_h3}`}>
+                    <a href="rishikesan070@gmail.com">
+                      rishikesan070@gmail.com
+                    </a>
+                  </h3>
+                </div>
+
+                <div className="d-flex align-items-end">
+                  <div className={`${styles.icon_div}`}>
+                    <IoMailOutline />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,53 +1,17 @@
-import React, { useRef, useState } from "react";
+// import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-scroll";
 
 function Navbar() {
-  const [nav, setNav] = useState(false);
+  // const [nav, setNav] = useState(false);
 
   return (
     <>
       <nav>
-        <div className={`container-fluid ${styles.nav_container}`}>
+        <div className={`container-fluid px-0 ${styles.nav_container}`}>
           <div className={`${styles.nav_div_parent}`}>
             <div className={`${styles.nav_brand}`}>
               <h5 className="">Rishi.</h5>
-            </div>
-
-            <div className={`${styles.hamburger_menu}`}>
-              <button
-                className="d-flex align-items-center"
-                onClick={() => setNav(!nav)}
-              >
-                <span className="uc px-1">Menu</span>{" "}
-                <div>
-                  <div className={`${styles.bar}`}></div>
-                  <div className={`${styles.bar}`}></div>
-                  <div className={`${styles.bar}`}></div>
-                </div>
-              </button>
-            </div>
-            <div
-              style={{
-                maxHeight: nav ? "200px" : 0,
-                transition: "max-height 0.5s ease-in-out",
-              }}
-              className={`${styles.nav_route_active} d-md-none d-block`}
-            >
-              <ul className={`${styles.nav_ul}`}>
-                <li onClick={() => setNav(!nav)}>
-                  <Link>Home</Link>
-                </li>
-                <li onClick={() => setNav(!nav)}>
-                  <Link>Work</Link>
-                </li>
-                <li onClick={() => setNav(!nav)}>
-                  <Link>Service</Link>
-                </li>
-                <li onClick={() => setNav(!nav)}>
-                  <Link>Contact</Link>
-                </li>
-              </ul>
             </div>
 
             <div className={`${styles.nav_route}  d-md-block d-none`}>
@@ -72,7 +36,6 @@ function Navbar() {
             </div>
           </div>
 
-          {/* <div className={`${styles.}`}></div> */}
         </div>
       </nav>
     </>
